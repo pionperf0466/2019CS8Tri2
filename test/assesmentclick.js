cb5fc8fb805821ff621e9e57509261a149b604c2
pionperf0466 <madebiyi24@nscds.org>
2020-02-05 17:14:20 +0000

    assesment

---
+++ test/assesmentclick.js
@@ -0,0 +1,12 @@
+$("box1").click(
+    function(){
+        alert("hello");
+  }
+);
+
+$("box4").click(
+    function(){
+        var clickedrannumline = $().css("rannumline");
+        alert(clickedrannumline);
+  }
+);
