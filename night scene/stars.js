a483cfad25796dc6e9510103a90a32a1f63e8f98
pionperf0466 <madebiyi24@nscds.org>
2020-02-07 15:24:50 +0000

    final

--- night scene/stars.js
+++ night scene/stars.js
@@ -1,6 +1,8 @@
-var randNum = ;
-console.log(randNum);
+var x = Math.floor(Math.random() * 1500);
+console.log(x);
 
+var y = Math.floor(Math.random() * 800);
+console.log(y);
 
 /*for (i = 1; i <= 100; i++) {
     $('body').append("<div class='star'></div>");
